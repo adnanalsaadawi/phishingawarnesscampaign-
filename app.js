36ec4703417b064d7647019a2d1c186e2f3bfb65
adnanalsaadawi <adnansaadawi3@icloud.com>
2024-12-05 15:23:09 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+// This function handles the button click to redirect users to phishing resources
+function goToResources() {
+    // Open a new window or tab with phishing awareness resources (you can replace with a real URL)
+    window.open('https://www.phishing.org/what-is-phishing', '_blank');
+}
